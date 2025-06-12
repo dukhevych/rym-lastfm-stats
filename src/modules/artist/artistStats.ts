9a6079e7a08a8bd57d9219537766ccaa212f8b18
Bohdan Dukhevych <landenmetal@gmail.com>
2025-06-12 16:39:01 +0300

    .

--- src/modules/artist/artistStats.ts
+++ src/modules/artist/artistStats.ts
@@ -1,47 +1,15 @@
 import * as utils from '@/helpers/utils';
+import * as constants from '@/helpers/constants';
 import * as api from '@/api';
 import { createElement as h } from '@/helpers/utils';
+import {
+  getArtistId,
+  getArtistNames,
+  PARENT_SELECTOR,
+  ARTIST_INFO_SELECTOR,
+} from './targets';
 
 import '@/modules/release/releaseStats.css';
-
-const PARENT_SELECTOR = '.artist_left_col';
-const ARTIST_INFO_SELECTOR = '.artist_info_main';
-const ARTIST_ID_SELECTOR = 'input.rym_shortcut';
-
-function getArtistId(parent: HTMLElement) {
-  const artistIdInput: HTMLInputElement = parent.querySelector(ARTIST_ID_SELECTOR)!;
-  return utils.extractIdFromTitle(artistIdInput.value);
-}
-
-function getArtistNames(parent: HTMLElement) {
-  let artistAkaNames: string[] = [];
-
-  const artistNameHeader = parent.querySelector('.artist_name_hdr');
-  const artistName = utils.getNodeDirectTextContent(artistNameHeader);
-  const artistNameHeaderSpan = artistNameHeader?.querySelector('span');
-  const artistNameLocalized = artistNameHeaderSpan?.textContent;
-
-  const artistAkaNamesElementHeader = Array.from(parent.querySelectorAll(ARTIST_INFO_SELECTOR + ' .info_hdr')).find(element => {
-    return element.textContent?.toLowerCase().includes('also known as');
-  });
-
-  if (artistAkaNamesElementHeader) {
-    const artistAkaNamesElement = artistAkaNamesElementHeader.nextElementSibling;
-    const artistAkaNamesText = artistAkaNamesElement?.querySelector('span')?.textContent;
-    if (artistAkaNamesText) {
-      artistAkaNamesText.split(', ').forEach(name => {
-        const nameCleaned = name.replace(/\s*\[birth name\]$/i, '').trim(); // Check for other patterns to remove
-        artistAkaNames.push(nameCleaned);
-      });
-    }
-  }
-
-  return {
-    artistName,
-    artistNameLocalized,
-    artistAkaNames,
-  };
-}
 
 interface UIElements {
   parent: HTMLElement;
@@ -52,24 +20,214 @@ interface UIElements {
   userplaycount: HTMLElement;
   lastfmLinksContainer: HTMLElement;
   lastfmLink: HTMLAnchorElement;
-  // lastfmLink2: HTMLAnchorElement;
   statsWrapper: HTMLElement;
   heading: HTMLElement;
   content: HTMLElement;
+  artistNamesDialog: HTMLDialogElement;
+  artistNamesDialogList: HTMLUListElement;
+  artistNamesDialogListItems: HTMLLIElement[];
+  artistNamesDialogOpener: HTMLAnchorElement;
 }
 
 interface State {
+  artistName: string;
+  artistNameLocalized: string;
+  artistAkaNames: string[];
+  artistId: string;
   artistQuery: string;
   artistQueryCacheKey: string;
   artistNameOptions: string[];
   notFound: boolean;
   userName: string;
-  storageKey: string;
+  cacheStorageKey: string;
 }
 
 let config: ProfileOptions;
 const uiElements = {} as UIElements;
-const state = {} as State;
+const state = {
+  get artistNameOptions(): string[] {
+    return [this.artistNameLocalized, this.artistName, ...this.artistAkaNames, 'asdsdsdsdssdosds0dsa0dsa'].filter(Boolean);
+  },
+  get artistQueryCacheKey(): string { return `artistQuery_${this.artistId}`; },
+  get cacheStorageKey(): string { return `artistStats_${this.artistId}_${this.artistQuery}`; },
+} as State;
+
+function createArtistNamesDialog() {
+  const list = h('ul', { className: 'list-dialog' });
+
+  const dialog = h('dialog', { className: 'dialog-base', id: 'dialog-artist-names' }, [
+    h(
+      'h2',
+      { className: 'dialog-title' },
+      [
+        'Choose artist name',
+        h(
+          'a',
+          {
+            href: '#',
+            className: 'dialog-close-btn',
+            onClick: (e: MouseEvent) => {
+              e.preventDefault();
+              dialog.close();
+            },
+          },
+          utils.createSvgUse('svg-close-symbol')
+        ),
+      ],
+    ),
+    list,
+  ]);
+
+  return { dialog, list };
+}
+
+async function updateArtistStats() {
+  let stats: ArtistStats | null = null;
+  let timestamp: number | null = null;
+
+  const cachedData = await utils.storageGet(state.cacheStorageKey, 'local');
+
+  const cacheLifetime = state.userName ? constants.STATS_CACHE_LIFETIME_MS : constants.STATS_CACHE_LIFETIME_GUEST_MS;
+
+  if (
+    cachedData
+      && cachedData.timestamp
+      && ((Date.now() - cachedData.timestamp) <= cacheLifetime)
+  ) {
+    console.log('Using cached data');
+
+    stats = cachedData.data;
+    timestamp = cachedData.timestamp;
+  }
+
+  if (!stats && !timestamp) {
+    console.log('Fetching new data');
+
+    stats = await fetchArtistInfo(state.artistQuery);
+    timestamp = Date.now();
+
+    await utils.storageSet({
+      [state.cacheStorageKey]: {
+        timestamp,
+        data: stats,
+      },
+    }, 'local');
+  }
+
+  uiElements.statsWrapper.classList.remove('is-loading');
+  uiElements.statsWrapper.classList.remove('no-data');
+
+  updateDialogState();
+
+  if (stats && timestamp) {
+    updateArtistInfo(stats, timestamp);
+  } else {
+    uiElements.statsWrapper.classList.add('no-data');
+  }
+}
+
+async function fetchArtistInfo(artistName: string) {
+  const artistInfoResponse = await api.getArtistInfo({
+    params: {
+      username: state.userName,
+      artist: artistName,
+    },
+    apiKey: config.lastfmApiKey || process.env.LASTFM_API_KEY as string,
+  });
+
+  const stats: ArtistStats = {
+    playcount: 0,
+    listeners: 0,
+    userplaycount: null,
+    url: '',
+  };
+
+  if (artistInfoResponse && !artistInfoResponse.error) {
+    stats.playcount = +artistInfoResponse.artist.stats.playcount;
+    stats.listeners = +artistInfoResponse.artist.stats.listeners;
+    stats.userplaycount = artistInfoResponse.artist.stats.userplaycount ? +artistInfoResponse.artist.stats.userplaycount : null;
+    stats.url = artistInfoResponse.artist.url;
+
+    return stats;
+  } else {
+    return null;
+  }
+}
+
+interface ArtistStats {
+  playcount: number;
+  listeners: number;
+  userplaycount?: number | null;
+  url: string;
+}
+
+function updateArtistInfo(stats: ArtistStats, timestamp: number) {
+  populateArtistStats(stats, timestamp);
+}
+
+function updateDialogState() {
+  if (!uiElements.artistNamesDialog) {
+    return;
+  }
+
+  uiElements.artistNamesDialogListItems.forEach(item => {
+    const itemArtistName = item.dataset.value;
+    if (itemArtistName === state.artistQuery) {
+      item.classList.add('is-selected');
+    } else {
+      item.classList.remove('is-selected');
+    }
+  });
+}
+
+function createArtistNamesDialogListItem(artistName: string) {
+  return h('li', { className: 'list-dialog-item', dataset: { value: artistName } }, [
+    h(
+      'a',
+      {
+        href: utils.generateLastFMProfileUrl(artistName),
+        className: 'list-dialog-item-link',
+        onClick: async (e: MouseEvent) => {
+          e.preventDefault();
+          state.artistQuery = artistName;
+          uiElements.statsWrapper.classList.add('is-updating');
+          utils.storageSet({
+            [state.artistQueryCacheKey]: state.artistQuery,
+          }, 'local'),
+          uiElements.artistNamesDialog.close();
+
+          await updateArtistStats();
+
+          uiElements.statsWrapper.classList.remove('is-updating');
+        },
+      },
+      artistName,
+    ),
+  ]);
+}
+
+function populateArtistNamesDialog() {
+  if (!uiElements.artistNamesDialogList || !uiElements.artistNamesDialog) return;
+
+  uiElements.artistNamesDialogList.replaceChildren();
+
+  if (!state.artistNameOptions || state.artistNameOptions.length === 0) {
+    const noResultsItem = h('li', { className: 'artist-name-item is-no-results' }, 'No results found');
+    uiElements.artistNamesDialogList.appendChild(noResultsItem);
+    return;
+  }
+
+  uiElements.artistNamesDialogListItems = state.artistNameOptions.map(createArtistNamesDialogListItem);
+  uiElements.artistNamesDialogList.append(...uiElements.artistNamesDialogListItems);
+}
+
+function prepareArtistNamesDialog() {
+  const { dialog, list } = createArtistNamesDialog();
+  uiElements.artistNamesDialogList = list;
+  uiElements.artistNamesDialog = dialog;
+
+  document.body.appendChild(uiElements.artistNamesDialog);
+}
 
 function prepareArtistStatsUI() {
   uiElements.statsList = h('ul', { className: 'list-stats' }, [
@@ -90,8 +248,27 @@ function prepareArtistStatsUI() {
   uiElements.statsWrapper = h(
     'div',
     { className: ['list-stats-wrapper', 'is-loading'] },
-    [uiElements.statsList, uiElements.lastfmLink]
+    [
+      uiElements.statsList,
+      uiElements.lastfmLink,
+    ]
   );
+
+  if (uiElements.artistNamesDialog) {
+    uiElements.artistNamesDialogOpener = uiElements.artistNamesDialog && h(
+      'a',
+      {
+        className: 'incorrect-stats-link',
+        onClick: (e: MouseEvent) => {
+          e.preventDefault();
+          uiElements.artistNamesDialog.showModal();
+        },
+      },
+      'Switch artist'
+    );
+
+    uiElements.statsWrapper.append(uiElements.artistNamesDialogOpener);
+  }
 
   uiElements.heading = h('div', {
     className: 'info_hdr',
@@ -107,24 +284,11 @@ function prepareArtistStatsUI() {
   uiElements.artistInfoMain.append(uiElements.heading, uiElements.content);
 }
 
-interface ArtistStats {
-  playcount: number;
-  listeners: number;
-  userplaycount?: number;
-  url: string;
-  notFound: boolean;
-}
-
 function populateArtistStats(
-  { playcount, listeners, userplaycount, url, notFound }: ArtistStats,
-  timestamp?: number,
+  stats: ArtistStats,
+  timestamp?: number | null,
 ) {
-  uiElements.statsWrapper.classList.remove('is-loading');
-
-  if (notFound) {
-    uiElements.statsWrapper.classList.add('not-found');
-    return;
-  }
+  const { playcount, listeners, userplaycount, url } = stats;
 
   const cacheTimeHint = timestamp ? `(as of ${new Date(timestamp).toLocaleDateString()})` : '';
 
@@ -147,7 +311,7 @@ function populateArtistStats(
   if (userplaycount !== undefined) {
     uiElements.userplaycount.style.display = 'block';
     uiElements.userplaycount.title = `${userplaycount} scrobbles`;
-    uiElements.userplaycount.textContent = `My scrobbles: ${utils.shortenNumber(Math.trunc(userplaycount))}`;
+    uiElements.userplaycount.textContent = `My scrobbles: ${utils.shortenNumber(Math.trunc(userplaycount || 0))}`;
   } else {
     uiElements.userplaycount.style.display = 'none';
   }
@@ -170,95 +334,37 @@ async function render(_config: ProfileOptions) {
 
   config = _config;
 
-  const { artistName, artistNameLocalized, artistAkaNames } = getArtistNames(uiElements.parent);
-  const artistId = getArtistId(uiElements.parent);
+  state.artistId = getArtistId(uiElements.parent);
 
-  if (!artistName && !artistNameLocalized) {
+  if (!state.artistId) {
+    console.warn('No artist ID found.');
+    return;
+  }
+
+  const { artistName, artistNameLocalized, artistAkaNames } = getArtistNames(uiElements.parent);
+  state.artistName = artistName;
+  state.artistNameLocalized = artistNameLocalized;
+  state.artistAkaNames = artistAkaNames;
+
+  if (!state.artistName && !state.artistNameLocalized) {
     console.error('No artist found.');
     return;
   }
 
-  state.artistNameOptions = [artistName];
-  if (artistNameLocalized) state.artistNameOptions.unshift(artistNameLocalized);
-  if (artistAkaNames.length > 0) state.artistNameOptions.push(...artistAkaNames);
-
-  state.artistQueryCacheKey = `artistQuery_${artistId}`;
-  state.artistQuery = await utils.storageGet(state.artistQueryCacheKey, 'local');
-
-  if (!state.artistQuery) {
-    state.artistQuery = state.artistNameOptions[0];
-  }
+  state.artistQuery = await utils.storageGet(state.artistQueryCacheKey, 'local') || state.artistName;
 
   const userData = await utils.getSyncedUserData();
   const userName = userData?.name;
   if (userName) state.userName = userName;
 
-  state.storageKey = `artistStats_${state.artistQuery}`;
+  if (state.artistNameOptions.length > 1) {
+    prepareArtistNamesDialog();
+  }
 
   prepareArtistStatsUI();
+  populateArtistNamesDialog();
 
-  if (!config.lastfmApiKey) {
-    const cachedData = await utils.storageGet(state.storageKey, 'local');
-
-    if (cachedData) {
-      const { timestamp, data } = cachedData;
-      const cachedDate = new Date(timestamp).toDateString();
-      const currentDate = new Date().toDateString();
-
-      if (cachedDate === currentDate) {
-        console.log('Inserting cached lastfm data:', data);
-        populateArtistStats(data, timestamp);
-        return;
-      }
-    }
-  }
-
-  const artistInfoResponse = await api.getArtistInfo({
-    params: {
-      username: userName,
-      artist: state.artistQuery,
-    },
-    apiKey: config.lastfmApiKey || process.env.LASTFM_API_KEY as string,
-  });
-
-  let playcount = 0;
-  let listeners = 0;
-  let userplaycount;
-  let url = null;
-  let notFound = false;
-
-  if (artistInfoResponse && !artistInfoResponse.error) {
-    playcount += +artistInfoResponse.artist.stats.playcount;
-    listeners += +artistInfoResponse.artist.stats.listeners;
-
-    if (artistInfoResponse.artist.stats.userplaycount) {
-      userplaycount = 0;
-      userplaycount += +artistInfoResponse.artist.stats.userplaycount;
-    }
-
-    url = artistInfoResponse.artist.url;
-  } else {
-    notFound = true;
-  }
-
-  const stats: ArtistStats = {
-    playcount,
-    listeners,
-    userplaycount,
-    url,
-    notFound,
-  };
-
-  if (!config.lastfmApiKey) {
-    await utils.storageSet({
-      [state.storageKey]: {
-        timestamp: Date.now(),
-        data: stats,
-      },
-    }, 'local');
-  }
-
-  populateArtistStats(stats);
+  await updateArtistStats();
 }
 
 export default {
